6f1f93165cc372fe5f98b73cfc3bef878abd6c92
Husain-Siddik <ronysiddik21@gmail.com>
2024-08-22 10:00:40 +0600

    project build and tailwindcss ,deasy ui added seccesfully

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  //DeasyUi requre red signal er jonno 
+  node:true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1 class="text-9xl ">This is an about page</h1>
+  </div>
+
+  <div>
+    
+  </div>
+</template>
+
+<style>
+
+</style>
