027876499cd81d1959634fc36859103ac6f38a68
Husain-Siddik <ronysiddik21@gmail.com>
2024-08-22 22:53:11 +0600

    Registraton succesful an user by Email and password

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,20 +1,47 @@
-
 import { defineStore } from "pinia";
+
+//Auth form Firebase Config and Other nesseary item for firebase
+import Auth from "@/Firebase/Firebase.config";
+import { createUserWithEmailAndPassword } from "firebase/auth"
+//
 
 export const UseUserStore = defineStore('user', {
     state: () => ({
+
         email: '',
         password: '',
+
     }),
     getters: {
-       // doubleCount: (state) => state.count * 2,
+        // doubleCount: (state) => state.count * 2,
     },
     actions: {
-        handleRegister(){
-           console.log('stored succesfully');
-           
-        }
-        
+
+        handleRegister() {
+            console.log('Clicked  succesfully')
+
+            //Create An user 
+            createUserWithEmailAndPassword(Auth, this.email, this.password)
+                .then((userCredential) => {
+                    // Signed up 
+                    const user = userCredential.user;
+                    console.log(user);
+                    
+                    // ...
+                })
+                .catch((error) => {
+                    //const errorCode = error.code;
+                    //const errorMessage = error.message;
+                    // ..
+                    console.log(error);
+                    
+                });
+
+
+
+
+        },
+
     },
 
 })
